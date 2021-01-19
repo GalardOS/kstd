b5fb1cf4546d7369dcc28dc1ecc6c436825904ff
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-01-19 09:12:47 +0100

    Fixed allocation size bug

--- src/memory/heap.cc
+++ src/memory/heap.cc
@@ -1,7 +1,14 @@
 #include "heap.hh"
 #include <iostream>
 
-#define DEBUG(x) std::cout << x << std::endl;
+#if 1
+    #define DEBUG(x) std::cout << x << std::endl;
+#else
+    #define DEBUG(x)
+#endif
+
+#define META_STATUS_FREE 0
+#define META_STATUS_FULL 1
 
 namespace kstd {
     struct meta_block {
@@ -22,10 +29,33 @@ namespace kstd {
         ((meta_block*)(memory_buffer))->status = 0;
     }
 
+    static void print_table() {
+        uint32 index = 0;
+        DEBUG("-----------------");
+        while(index <= memory_size) {
+            meta_block* meta = (meta_block*)(memory_buffer + index);
+
+            DEBUG("Meta block in index " << index);
+            DEBUG("   size   =  " << meta->size_in_blocks);
+            DEBUG("   status =  " << meta->status);
+
+            index += meta->size_in_blocks + 1;
+        }
+
+        DEBUG("-----------------");
+    }
+
     void* alloc(uint32 size) {
         uint32 index = 0;
-        uint32 size_in_blocks = size / 8;
 
+        // Calculate the number of blocks to store the data, as
+        // the size cant be 0, is manually set to 1 if necessary
+        uint32 size_in_blocks = size / 8;
+        if(size_in_blocks == 0) {
+            size_in_blocks = 1;
+        }
+
+        DEBUG("----------------------------------------");
         DEBUG("Allocating " << size_in_blocks << " blocks of memory");
 
         while(true) {
@@ -36,7 +66,7 @@ namespace kstd {
             DEBUG("  size = " << meta->size_in_blocks);
             DEBUG("  free = " << meta->status);
 
-            if(meta->status == 0 && meta->size_in_blocks > size) {
+            if(meta->status == 0 && meta->size_in_blocks >= size_in_blocks) {
                 DEBUG("Block is free");
                 
                 // Update the free control block to full
@@ -46,13 +76,15 @@ namespace kstd {
                 // Set the next empty node
                 ((meta_block*)(memory_buffer + index + size_in_blocks + 1))->size_in_blocks = memory_size - size_in_blocks - 1;
                 ((meta_block*)(memory_buffer + index + size_in_blocks + 1))->status = 0;
-                
+
+                DEBUG("----------------------------------------");
                 return memory_buffer + index + 1;
             } else {
                 index += meta->size_in_blocks + 1;
                 DEBUG("Not free, next index to search : " << index);
             }
         }
+
 
         return nullptr;
     }
@@ -62,6 +94,7 @@ namespace kstd {
         meta_block* control_block = static_cast<meta_block*>(ptr);
         control_block--;
 
-        control_block->status = 0;
+        // Set the status to free
+        control_block->status = META_STATUS_FREE;
     }
 }
